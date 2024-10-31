1459124930db3e539d1b727856591d3d3c903591
AnnaZaitseva05 <sharkoanna5@gmail.com>
2024-10-31 11:27:19 +0300

    Create module_4_2.py

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+test_function()
+inner_function() # вызов этой функции inner_function() вне функции test_function вызовет ошибку, так как она находится внутри другой функции, у нее другая область видимости
